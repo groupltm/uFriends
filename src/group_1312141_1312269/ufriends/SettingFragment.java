c95f7ea75e9c3958e5893e134230c7dabcf70569
ngochung1243 <ngochung1243@gmail.com>
2016-05-03 19:01:48 +0700

    ok

---
+++ src/group_1312141_1312269/ufriends/SettingFragment.java
@@ -0,0 +1,25 @@
+package group_1312141_1312269.ufriends;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingFragment extends Fragment{
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	@Nullable
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+}
