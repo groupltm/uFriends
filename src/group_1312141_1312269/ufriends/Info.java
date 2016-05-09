b3d035c09fdc8c90964956bd961cdb3899e3be85
ngochung1243 <ngochung1243@gmail.com>
2016-05-09 21:10:28 +0700

    working setting fragment

--- src/group_1312141_1312269/ufriends/Info.java
+++ src/group_1312141_1312269/ufriends/Info.java
@@ -3,21 +3,21 @@ package group_1312141_1312269.ufriends;
 import java.io.Serializable;
 
 public class Info implements Serializable {
-	byte[] _image;
+	String _imagePath;
 	String _name;
 
 	int _age;
 	boolean _sex;
 
 	public Info() {
-		_image = null;
+		_imagePath = "";
 		_name = "anonymous";
 		_age = 0;
 		_sex = false;
 	}
 	
-	public void setInfo(byte[] image, String name, int age, boolean sex){
-		_image = image;
+	public void setInfo(String imagePath, String name, int age, boolean sex){
+		_imagePath = imagePath;
 		_name = name;
 		_age = age;
 		_sex = sex;
